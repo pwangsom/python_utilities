8988a2d30f296922d54e6a9741f93dbcd2bfd48c
Peerasak Wangsom <pwangsom@gmail.com>
2018-12-20 15:42:28 +0700

    Update extract_a5_indicator_last_gen.py

--- workflow_scheduling_for_journal/extract_a5_indicator_last_gen.py
+++ workflow_scheduling_for_journal/extract_a5_indicator_last_gen.py
@@ -1,6 +1,27 @@
 import glob
 import csv
 
+def getSpecificedLine(file):
+    collector = []
+    with open(file) as infile:
+        i = 1
+        for line in infile:
+            fields = line.replace('\n','').replace('\r', '').split(',')
+            if (i != 1 and int(fields[5]) == int(fields[6])) :
+                collector.append(fields)
+                # print(fields)
+                # print(collector)
+            i += 1
+
+    collector = sorted(collector, key=lambda x: (x[2], x[3]))
+
+    return collector    
+
+def saveToCsvFile(collector, file):
+    with open(file, mode='wt', newline='') as myfile:
+        writer = csv.writer(myfile, dialect='excel')
+        writer.writerows(collector)
+
 # Main
 
 # D:\Users\Peerasak\Google Drive KMUTT\PhD Works\Experiments\journal\journal01\output
@@ -23,7 +44,7 @@ file_list = []
 
 for file in out_files:
     file = file.replace('\\', '/')
-    print(str(i) + " " + file)
+    # print(str(i) + " " + file)
 
     file_name = file.rsplit('/', 1)[1].split('_')
 
@@ -40,7 +61,33 @@ file_list = sorted(file_list, key=lambda x: (x[1], int(x[2])))
 
 print("")
 
+content = []
+
 i = 1
 for file in file_list:
     print(str(i) + " " + file[0])
+
+    if(i == 1):
+        with open(file[0], 'r') as f:
+            head = f.readline().replace('\n','').replace('\r', '')
+            head = head.split(',')
+            # print(head)
+
+            content.append(head)
+
+        content.extend(getSpecificedLine(file[0]))
+    else:
+        content.extend(getSpecificedLine(file[0]))
+
+    #print(content)
+
     i += 1
+
+
+saveAsFile = source_dir.replace('\\', '/')
+saveAsFile = saveAsFile.rsplit('/', 1)[0]
+saveAsFile = saveAsFile + '/' + experiment + '_a5_indicator_all.csv'
+
+print(saveAsFile)
+
+saveToCsvFile(content, saveAsFile)
