390374729eea992e1a3844f4a0bf42e5e72984d6
Peerasak Wangsom <pwangsom@gmail.com>
2019-01-15 11:11:41 +0700

    Create extract_a5_to_csv.py

--- analyze_multipleinput/extract_convert_a5_to_csv.py
+++ analyze_multipleinput/extract_convert_a5_to_csv.py
@@ -27,14 +27,24 @@ def removeUnwantedCharacter(line):
 
 def readFile(file, mode):
     collector = []
+
+    i = 1
+
     with open(file) as infile:
         for line in infile:
             if(mode == 1):
                 collector.append(removeUnwantedCharacter(line))
             elif(mode == 2):
-                fields = line.replace('\n','').replace('\r', '').split(',')
-                if(int(fields[5]) == int(fields[6])):
+                if(i == 1):
+                    fields = line.replace('\n','').replace('\r', '').split(',')
                     collector.append(fields)
+                else:
+                    skip = [98, 99]
+                    fields = line.replace('\n','').replace('\r', '').split(',')
+                    if(int(fields[5]) == int(fields[6]) and int(fields[4]) not in skip):
+                        collector.append(fields)
+            
+            i = i+1
 
     return collector
 
@@ -51,10 +61,10 @@ def getWindowDirectory():
     # pwangsom, Peerasak
     window_user = ['pwangsom', 'Peerasak']
 
-    window_dir = 'D:/Users/' + window_user[0] + '/Google Drive KMUTT/PhD Works/'
+    window_dir = 'D:/Users/' + window_user[0] + '/Google Drive KMUTT/PhD Works'
 
     if(os.path.exists(window_dir) == False):
-        window_dir = 'D:/Users/' + window_user[1] + '/Google Drive KMUTT/PhD Works/'
+        window_dir = 'D:/Users/' + window_user[1] + '/Google Drive KMUTT/PhD Works'
 
     return window_dir
 
@@ -97,8 +107,10 @@ def extractLastGenFromCsv(source_dir, dest_dir):
         file = file.replace('\\', '/')
         print(str(i) + " " + file)
 
-        saveAsFile = dest_dir + file.rsplit('/', 1)[1]
-        saveAsFile = saveAsFile.replace('a3_objective', 'a3_lastgen_objective')
+        split_file_name = file.rsplit('/', 4)
+
+        saveAsFile = dest_dir + split_file_name[1] + "/experiment0/output/" + split_file_name[4]
+        saveAsFile = saveAsFile.replace('a5_indicator', 'a5_lastgen_indicator')
 
         saveToCsvFile(readFile(file, 2), saveAsFile)
 
@@ -112,21 +124,19 @@ def extractLastGenFromCsv(source_dir, dest_dir):
 
 # Main
 
-experiment = 'pilot01'
+experiment = 'multipleinput'
 
 # source_dir = 'F:/My_PhD_Works/Experiments/' + experiment + '/output/*/*a3_objective.out'
-main_source = 'D:/Users/pwangsom/My_PhD_Works/Experiments/' + experiment + '/output/*/'
+main_source = 'F:/My_PhD_Works/Experiments/' + experiment + '/*/output/*/'
 
-dest_dir = getWindowDirectory() + '/Experiments/'+ experiment + '/output/'
-
+dest_dir = getWindowDirectory() + '/Experiments/'+ experiment + '/'
 
 # Step 1. convert a3.out to a3.csv
-source_dir = main_source + '*a3_objective.out'
+source_dir = main_source + '*a5_indicator.out'
 convertOutToCsv(source_dir)
 
-
 # Step 2. extract last gen from a3.csv
-source_dir = main_source + '*a3_objective.csv'
+source_dir = main_source + '*a5_indicator.csv'
 extractLastGenFromCsv(source_dir, dest_dir)
 
 #######################################################
