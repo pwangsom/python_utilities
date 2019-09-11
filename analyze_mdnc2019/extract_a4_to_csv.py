3b3fcf2e02e41d4c634a853e4e5a12a6b683535d
Peerasak Wangsom <pwangsom@gmail.com>
2019-09-11 09:37:05 +0700

    Update extract_a4_to_csv.py

--- analyze_mdnc2019/extract_a4_to_csv.py
+++ analyze_mdnc2019/extract_a4_to_csv.py
@@ -18,7 +18,14 @@ def getWindowDirectory():
 
 #######################################################
 
-def convertOutToCsv(source_dir):
+def saveToCsvFile(collector, file):
+    with open(file, mode='wt') as myfile:
+        writer = csv.writer(myfile, lineterminator='\n')
+        writer.writerows(collector)
+
+#######################################################
+
+def convertOutToCsv(source_dir, dest_dir):
 
     print(source_dir)
     out_files = glob.glob(source_dir)
@@ -31,11 +38,21 @@ def convertOutToCsv(source_dir):
         file = file.replace('\\', '/')
         print(str(i) + " " + file)
 
-        saveAsFile = file.replace(".out", ".csv")
+        new_file_name = file.rsplit('/', 1)[1]
+        new_file_name = new_file_name.replace("runALL_maxgen300_genLAST_all_normalized_objective.out", "archive.csv")
+        
+        new_file = dest_dir + new_file_name
 
-        saveToCsvFile(readFile(file), saveAsFile)
+        collector = []
 
-        print(str(i) + " " + saveAsFile)
+        with open(file) as infile:
+            for line in infile:
+                line = line.replace(", ", ",")[:-2]
+                collector.append(line.split(','))
+
+        saveToCsvFile(collector, new_file)
+
+        print(str(i) + " " + new_file)
         print("")
 
         i += 1
@@ -48,9 +65,9 @@ experiment = 'MDNC2019/max30runs'
 
 main_source = 'D:/Users/pwangsom/Experiments/' + experiment + '/output/*/'
 
-dest_dir = getWindowDirectory() + '/Experiments/'+ experiment + '/experiment0/output/'
+dest_dir = getWindowDirectory() + '/Experiments/'+ experiment + '/experiment0/output/archive/'
 
 # Step 1. convert a5.out to a5.csv
 print("Step 1. convert a5.out to a5.csv")
-source_dir = main_source + '*a5_indicator.out'
-convertOutToCsv(source_dir)
+source_dir = main_source + '*runALL_maxgen300_genLAST_all_normalized_objective.out'
+convertOutToCsv(source_dir, dest_dir)
